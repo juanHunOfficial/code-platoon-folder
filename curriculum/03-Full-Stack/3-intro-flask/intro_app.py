8d2c5e783bbe890b92be0888890d0dc3c9b37289
JUAN_HUN <juanhun2424@gmail.com>
2024-10-23 17:01:56 -0400

    revise current app.route functions to use list comprehension, add address and enrollments

--- curriculum/03-Full-Stack/3-intro-flask/intro_app.py
+++ curriculum/03-Full-Stack/3-intro-flask/intro_app.py
@@ -20,37 +20,66 @@ class Classes(db.Model):
     name = db.Column(db.String(50))
     credits = db.Column(db.Integer)
     
+class Enrollments(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.Integer)
+    class_id = db.Column(db.Integer)
+    grade = db.Column(db.String(1))
+    
+class Addresses(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    line_1 = db.Column(db.String(50))
+    city = db.Column(db.String(25))
+    state = db.Column(db.String(25))
+    zipcode = db.Column(db.String(5))
+
+@app.route("/enrollments", methods=["GET"])
+def get_enrollments():
+    enrollments = Enrollments.query.all()
+    formatted_enrollments = [{
+        "id": enroll.id,
+        "student_id": enroll.student_id,
+        "class_id": enroll.class_id,
+        "grade": enroll.grade        
+    } for enroll in enrollments ]
+    return jsonify(formatted_enrollments)
+    
+@app.route("/addresses", methods=["GET"])
+def get_addresses():
+    addresses = Addresses.query.all()
+    formatted_addresses = [{
+        "id": address.id,
+        "line_1": address.line_1,
+        "city": address.city,
+        "state": address.state,
+        "zipcode": address.zipcode
+    }for address in addresses]
+    
+    return jsonify(formatted_addresses)
+    
 @app.route("/classes", methods=["GET"])
 def get_classes():
     classes = Classes.query.all()
-    formatted_classes = []
-    for clss in classes:
-        formatted_classes.append(
-            {
-                "id": clss.id,
-                "name": clss.name,
-                "credits": clss.credits
-            }
-        )
+    formatted_classes = [{
+        "id": clss.id,
+        "name": clss.name,
+        "credits": clss.credits
+    }for clss in classes]
     return jsonify(formatted_classes)
     
-    def __repr__(self):
-        return self.first_name
+# def __repr__(self):
+#     return self.first_name
 
 @app.route("/students", methods=["GET"])  # GET PUT POST DELETE
 def get_students():
     students = Students.query.all()  # SELECT * FROM student;
-    formatted_students = []
-    for stud in students:
-        formatted_students.append(
-            {
-                "id": stud.id,
-                "first_name": stud.first_name,
-                "last_name": stud.last_name,
-                "birthdate": stud.birthdate,
-                "address_id": stud.address_id
-            }
-        )
+    formatted_students = [{
+        "id": stud.id,
+        "first_name": stud.first_name,
+        "last_name": stud.last_name,
+        "birthdate": stud.birthdate,
+        "address_id": stud.address_id
+    }for stud in students]
     return jsonify(formatted_students)
 
 
