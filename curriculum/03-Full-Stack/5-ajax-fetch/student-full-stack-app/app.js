3fd55195a48dfa79b20743c342f05de21ce27476
JUAN_HUN <juanhun2424@gmail.com>
2024-10-26 10:40:00 -0400

    manipulate the serach bar it incoporate some exception handling without quiting the program

--- curriculum/03-Full-Stack/5-ajax-fetch/student-full-stack-app/app.js
+++ curriculum/03-Full-Stack/5-ajax-fetch/student-full-stack-app/app.js
@@ -1,13 +1,35 @@
 const fetchFromFlask = (evt) =>{
     evt.preventDefault()
-    let p_tag = document.querySelector("#test-connection")
-    const flask_url = "http://127.0.0.1:8000"
-    fetch(flask_url+"/students")
+    const formData = new FormData(evt.target)
+    const formProps = Object.fromEntries(formData)
+    const input_field = document.querySelector("#input-field")
+    let p_tag = document.querySelector("#response-field")
+    let search_entry = formProps["entry"].trim()
+    input_field.value = ""
+    
+
+
+    if(search_entry.includes("students")){
+        search_entry = "students"
+    }else if(search_entry.includes("enrollments")){
+        search_entry = "enrollments"
+    }else if(search_entry.includes("classes")){
+        search_entry = "classes"
+    }else if(search_entry.includes("addresses")){
+        search_entry = "addresses"
+    }else{
+        p_tag.innerText = "Sorry an invalid entry was given please check your spelling."
+        return
+    }
+    
+    console.log(`--${search_entry}--`)
+    const flask_url = `http://127.0.0.1:8000/${search_entry}`
+    fetch(flask_url)
         .then((response) => response.json())
             .then((data) => {
                 console.log(data)
                 Object.keys(data["1"]).forEach(key => {
-                    p_tag.innerText += `${key}: ${data["1"][key]}\n`
+                    p_tag.innerText = `${key}: ${data["1"][key]}\n`
                 })
             })
             .catch(error => {
