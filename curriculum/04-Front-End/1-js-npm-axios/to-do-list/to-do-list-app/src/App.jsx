e020bf8d9c0af277b1b570237a8f601060dbabba
JUAN_HUN <juanhun2424@gmail.com>
2024-10-28 16:11:01 -0400

    add the ability to remove the list items that have been completed, still no styling to add

--- curriculum/04-Front-End/1-js-npm-axios/to-do-list/to-do-list-app/src/App.jsx
+++ curriculum/04-Front-End/1-js-npm-axios/to-do-list/to-do-list-app/src/App.jsx
@@ -1,24 +1,35 @@
+import React, {useState} from "react"
 import './App.css'
 
-const createEntry = (userInput) => {
-  let taskList = document.querySelector("#list-of-tasks")
-  let newTask = document.createElement("li")
-  newTask.className = "list-items"
-  newTask.innerText = userInput
-  taskList.appendChild(newTask)
-  document.querySelector("#input-field").value = ""
-}
+// const createEntry = (userInput) => {
+//   let taskList = document.querySelector("#list-of-tasks")
+//   let newTask = document.createElement("li")
+//   newTask.className = "list-items"
+//   newTask.innerText = userInput
+//   taskList.appendChild(newTask)
+//   
+// }
 
-const handleSubmit = (evt) => {
-  evt.preventDefault()
-  let input = document.querySelector("#input-field")
-  let userInput = input.value 
-  createEntry(userInput)
-}
+
 
 function App() {
-    
+    const [tasks, setTasks] = useState([]);
 
+    const handleSubmit = (evt) => {
+      evt.preventDefault()
+      let input = document.querySelector("#input-field")
+      let userInput = input.value 
+      document.querySelector("#input-field").value = ""
+      createEntry(userInput)
+    }
+
+    const createEntry = (userInput) => {
+        setTasks(prevTaskInstance => [...prevTaskInstance, userInput]);
+    }
+
+    function handleRemoveTask(index){
+      setTasks(tasks.filter((_, currentIndex) => currentIndex !== index));
+    }
 
   return (
     <>
@@ -28,7 +39,10 @@ function App() {
       </form>
       <div id="tasks-container" >
         <ul id="list-of-tasks" >
-
+          {tasks.map((task, index) => 
+          <li key={index} onClick={() => handleRemoveTask(index)}>
+            {task}
+          </li>)}
         </ul>
       </div>
       
