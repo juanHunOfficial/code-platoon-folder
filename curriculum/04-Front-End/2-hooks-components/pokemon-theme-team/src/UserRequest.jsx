01b8378009423b96ee8e06953b66b5fe02f4a660
JUAN_HUN <juanhun2424@gmail.com>
2024-10-29 15:17:19 -0400

    Refactor code to build a team based off the type(s) of the first pokemon retrieved

--- curriculum/04-Front-End/2-hooks-components/pokemon-theme-team/src/UserRequest.jsx
+++ curriculum/04-Front-End/2-hooks-components/pokemon-theme-team/src/UserRequest.jsx
@@ -4,42 +4,55 @@ import "./UserInput.css"
 
 function UserRequest() {
     const [pokemonImgs, setPokemonImgs] = useState([])
-    
-    const fetchImg = async(input) => {
-        let {data} = await axios.get(`https://pokeapi.co/api/v2/pokemon/${input}`)
-        setPokemonImgs(previousStateOfImgs => [...previousStateOfImgs, data.sprites.front_default])
-    }
-
-    const handleInput = (evt) => {
-        evt.preventDefault()
-        let input = document.querySelector("#pokemon-search-field").value
-        document.querySelector("#pokemon-search-field").value = ""
-        fetchImg(input)
-    }
 
     const changeTheState = (index) => {
         setPokemonImgs(pokemonImgs.filter((_, currentIndex) => currentIndex !== index))
     }
 
+    const setTheRestOfTheTeam = async(currentTypes) => {
+        let count = 1
+        while (count < 6){
+            const randomNumber = Math.floor(Math.random() * 1000 + 1)
+            let {data} = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`)
+            if(data.types.length === 2 && (currentTypes[0] === data.types[0].type.name && currentTypes[1] === data.types[1].type.name)){
+                setPokemonImgs(previousStateOfImgs => [...previousStateOfImgs, data.sprites.front_default])
+                count++
+            }else if (data.types.length === 1 && currentTypes[0] === data.types[0].type.name){
+                setPokemonImgs(previousStateOfImgs => [...previousStateOfImgs, data.sprites.front_default])
+                count++
+            }
+            
+        }
+    }
+
     const handleClick = async() =>{
-        const randomNumber = Math.floor(Math.random() * 1000)
+        const randomNumber = Math.floor(Math.random() * 1000 + 1)
         let {data} = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`)
+        const currentTypes = []
+        if(data.types.length === 2){
+            currentTypes.push(data.types[0].type.name, data.types[1].type.name)
+        }else{
+            currentTypes.push(data.types[0].type.name)
+        }
         setPokemonImgs(previousStateOfImgs => [...previousStateOfImgs, data.sprites.front_default])
+        setTheRestOfTheTeam(currentTypes)
     }
 
     return (
 
         <>
-            <h1>Build Your Pokemon Team!</h1>
-            <form onSubmit={handleInput}>
-                <input id='pokemon-search-field' type='text' placeholder='Enter the name of your pokemon'/>
-                <input type='submit' value="Add Pokemon!" />
-            </form>
-            <button id='pokemon-randomizer' onClick={handleClick} >Get Random Pokemon</button>
-            {pokemonImgs.map((pokemon, index) =>
-                <img src={pokemon} key={index} onClick={() => {changeTheState(index)}}>
-                </img>
-            )}
+            <h1 id='title'>Build Your Pokemon Team!</h1>
+
+            <div id='pokemon-imgs-container'>
+                {pokemonImgs.map((pokemon, index) =>
+                    <img src={pokemon} key={index} onClick={() => {changeTheState(index)}}>
+                    </img>
+                )}
+            </div>
+
+            <div id='randomize-btn-container'>
+                <button id='pokemon-randomizer' onClick={handleClick} >Get Your Team</button>
+            </div>
         </>
     )
 }
