60238bcca647d60cb061e72b7e0c443b72e29a48
JUAN_HUN <juanhun2424@gmail.com>
2024-10-31 09:50:59 -0400

    fix the dimensions of the first card and began styling

--- curriculum/04-Front-End/3-rwd/linkedIn-clone/src/components/ProfileHeader.jsx
+++ curriculum/04-Front-End/3-rwd/linkedIn-clone/src/components/ProfileHeader.jsx
@@ -1,15 +1,15 @@
 import CoverPhoto from "./CoverPhoto"
 import ProfilePhoto from "./ProfilePhoto"
 import ProfileInfo from "./ProfileInfo"
-
+import "./components-styles.css"
 
 function ProfileHeader () {
     return(
-        <>
-        <CoverPhoto/>
-        <ProfilePhoto/>
-        <ProfileInfo/>
-        </>
+        <div id="profile-header-container">
+            <CoverPhoto/>
+            <ProfilePhoto/>
+            <ProfileInfo/>
+        </div>
     )
 }
 
