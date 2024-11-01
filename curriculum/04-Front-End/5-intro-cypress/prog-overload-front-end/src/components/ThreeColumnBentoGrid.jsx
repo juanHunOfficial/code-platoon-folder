17fb970142f5bcd2fe9875fe0c8791ed9c2a4b7c
JUAN_HUN <juanhun2424@gmail.com>
2024-11-01 13:56:48 -0400

    update app title screen, basic html

--- curriculum/04-Front-End/5-intro-cypress/prog-overload-front-end/src/components/ThreeColumnBentoGrid.jsx
+++ curriculum/04-Front-End/5-intro-cypress/prog-overload-front-end/src/components/ThreeColumnBentoGrid.jsx
@@ -26,7 +26,7 @@ function ThreeColumnBentoGrid(){
                             <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)]">
                                 <div className="px-8 pt-8 sm:px-10 sm:pt-10">
                                     <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">Performance</p>
-                                    <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">Lorem ipsum, dolor sit amet consectetur adipisicing elit maiores impedit.</p>
+                                    <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">Stay at the top of your game and always know how much progress you have made towards your goal.</p>
                                 </div>
                                 <div className="flex flex-1 items-center justify-center px-8 max-lg:pb-12 max-lg:pt-10 sm:px-10 lg:pb-2">
                                     <img className="w-full max-lg:max-w-xs" src="https://tailwindui.com/plus/img/component-images/bento-03-performance.png" alt=""/>
