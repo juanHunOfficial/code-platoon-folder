73e4f134cade32d6a0e4c428bd6599d066aab5c6
JUAN_HUN <juanhun2424@gmail.com>
2024-11-01 11:03:54 -0400

    utilize keyframe and other starter animation redering with taildwind css to make a holographic effect for my card

--- curriculum/04-Front-End/4-component-utilities/pokemon-lookup/my-project/src/App.jsx
+++ curriculum/04-Front-End/4-component-utilities/pokemon-lookup/my-project/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import './index.css'
 import PokemonImg from './components/PokemonImg'
 import RequestButton from './components/RequestButton'
 import { useState } from 'react'
@@ -12,11 +13,12 @@ function App() {
   console.log(fetchedPokemon)
   return (
     <>
-      <div className='container z-0 mx-auto flex mt-32 justify-center items-center border rounded-xl h-96 w-72 relative bg-indigo-600' >
+      <div className='container animate-shine shadow-lg z-0 mx-auto flex mt-32 justify-center items-center border-4 border-black rounded-xl h-96 w-72 relative card-background overflow-hidden ' >
+      <div className='absolute inset-0 holographic-overlay animate-shine pointer-events-none' ></div>
         <PokemonImg fetchedImg={fetchedImg}/>
         <h2 className='absolute text-center font-semibold text-3xl z-30 w-auto top-4'>{fetchedPokemon.name}</h2>
         <h4 className='absolute text-center font-semibold text-2xl z-30 top-72 w-auto' >{fetchedTypes}</h4>
-      </div>
+      </div> 
       <div className='flex justify-center mt-10'>
       <RequestButton setFetchedPokemon={setFetchedPokemon} setFetchedTypes={setFetchedTypes} setFetchedImg={setFetchedImg} />
       </div>
--- curriculum/04-Front-End/4-component-utilities/pokemon-lookup/my-project/src/components/PokemonImg.jsx
+++ curriculum/04-Front-End/4-component-utilities/pokemon-lookup/my-project/src/components/PokemonImg.jsx
@@ -8,7 +8,7 @@ function PokemonImg({fetchedImg}){
     return(
         
         <>
-            <img className="border rounded-xl h-48 w-48 mt-10 absolute top-5 bg-slate-950 " src={fetchedImg} alt="An image of your requested pokemon" />
+            <img className="border rounded-xl h-48 w-48 mt-10 absolute top-5 bg-slate-600 " src={fetchedImg} alt="An image of your requested pokemon" />
         </>
     )
 }
