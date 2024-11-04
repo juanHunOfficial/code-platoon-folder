7d16a800e0c0c31916ff00e9e9f503744c702c9d
JUAN_HUN <juanhun2424@gmail.com>
2024-11-04 15:40:02 -0500

    prep character info page for intergrating a useEffect to fetch the api data upon rendering

--- curriculum/04-Front-End/6-intro-to-react-router/rick-and-morty-website/src/pages/CharacterInfoPage.jsx
+++ curriculum/04-Front-End/6-intro-to-react-router/rick-and-morty-website/src/pages/CharacterInfoPage.jsx
@@ -1,13 +1,19 @@
 import { useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+import axios from "axios"
 
 function CharacterInfoPage(){
     const location = useLocation();
     const charData = location.state;
 
+    const getData = () => {
+
+    }
+
     console.log(charData)
     return(
         <>
-            <h1>You made it to the char info page</h1>
+            <h1  >You made it to the char info page</h1>
         </>
     )
 }
