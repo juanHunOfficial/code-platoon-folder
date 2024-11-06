f370fe6d7c2af3af9011024e997d28883e68a10b
JUAN_HUN <juanhun2424@gmail.com>
2024-11-06 15:41:17 -0500

    create the db for the game_store assignment and added all the data needed

--- curriculum/05-RDBMS/1-schemadesign/game_store_db/game_store.sql
+++ curriculum/05-RDBMS/1-schemadesign/game_store_db/game_store.sql
@@ -1,34 +1,39 @@
 DROP TABLE IF EXISTS employees;
 CREATE TABLE employees (
     id SERIAL PRIMARY KEY,
-    employee_name VARCHAR(30),
-    position VARCHAR(30),
+    employee_name VARCHAR(50),
+    position VARCHAR(50),
     salary decimal(8,2)
 );
 
+COPY employees FROM '/Users/juanhun/Desktop/code-platoon-assignments/curriculum/05-RDBMS/1-schemadesign/example/data/employee.csv' WITH CSV HEADER; 
+
 DROP TABLE IF EXISTS games;
 CREATE TABLE games(
-    id SERIAL PRIMARY KEY,
-    game_id SERIAL, 
-    game_title VARCHAR(30),
+    game_id SERIAL PRIMARY KEY, 
+    game_title VARCHAR(50),
     quantity INT,
     price DECIMAL(5,2)
 );
+
+COPY games FROM '/Users/juanhun/Desktop/code-platoon-assignments/curriculum/05-RDBMS/1-schemadesign/example/data/game.csv' WITH CSV HEADER;
 
 DROP TABLE IF EXISTS action_figures;
 CREATE TABLE action_figures(
     id SERIAL PRIMARY KEY,
-    action_figure_title VARCHAR(30),
+    action_figure_title VARCHAR(50),
     quantity INT,
     price DECIMAL(5,2)
 );
 
+COPY action_figures FROM '/Users/juanhun/Desktop/code-platoon-assignments/curriculum/05-RDBMS/1-schemadesign/example/data/action_figure.csv' WITH CSV HEADER;
+
 DROP TABLE IF EXISTS posters;
 CREATE TABLE posters(
     id SERIAL PRIMARY KEY,
-    poster_title VARCHAR(30),
+    poster_title VARCHAR(50),
     quantity INT,
     price DECIMAL(4,2)
 );
 
-
+COPY posters FROM '/Users/juanhun/Desktop/code-platoon-assignments/curriculum/05-RDBMS/1-schemadesign/example/data/poster.csv' WITH CSV HEADER;
