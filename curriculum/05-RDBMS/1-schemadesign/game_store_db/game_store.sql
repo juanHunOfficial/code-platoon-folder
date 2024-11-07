b586f51fa3274949ce22ebf00f45400e7f03eeed
JUAN_HUN <juanhun2424@gmail.com>
2024-11-07 11:30:18 -0500

    add constraints to the game store sql file

--- curriculum/05-RDBMS/1-schemadesign/game_store_db/game_store.sql
+++ curriculum/05-RDBMS/1-schemadesign/game_store_db/game_store.sql
@@ -3,7 +3,12 @@ CREATE TABLE employees (
     id SERIAL PRIMARY KEY,
     employee_name VARCHAR(50),
     position VARCHAR(50),
-    salary decimal(8,2)
+    salary decimal(8,2),
+    CHECK(employee_name ~ '^[A-Za-z''-]+'),
+    CHECK(position IN ('Sales Associate', 'Store Manager', 'Inventory Clerk', 
+                       'Customer Service Representative', 'IT Specialist', 'Marketing Coordinator',
+                       'Assistant Manager', 'Finance Analyst', 'Security Officer', 'HR Coordinator')),
+    CHECK(salary BETWEEN 32000.00 and 60000.00)
 );
 
 COPY employees FROM '/Users/juanhun/Desktop/code-platoon-assignments/curriculum/05-RDBMS/1-schemadesign/example/data/employee.csv' WITH CSV HEADER; 
@@ -20,8 +25,11 @@ COPY games FROM '/Users/juanhun/Desktop/code-platoon-assignments/curriculum/05-R
 CREATE TABLE action_figures(
     id SERIAL PRIMARY KEY,
     action_figure_title VARCHAR(50),
-    quantity INT,
-    price DECIMAL(5,2)
+    quantity INT DEFAULT 1,
+    price DECIMAL(5,2),
+    CHECK(action_figure_title ~ '^[A-Za-z0-9!?/:''\-\s,;().\[\]{}&|0-9]+$'),
+    CHECK(quantity BETWEEN 1 AND 30),
+    CHECK(price BETWEEN 10 AND 100)
 );
 
 COPY action_figures FROM '/Users/juanhun/Desktop/code-platoon-assignments/curriculum/05-RDBMS/1-schemadesign/example/data/action_figure.csv' WITH CSV HEADER;
@@ -29,8 +37,11 @@ COPY action_figures FROM '/Users/juanhun/Desktop/code-platoon-assignments/curric
 CREATE TABLE posters(
     id SERIAL PRIMARY KEY,
     poster_title VARCHAR(50),
-    quantity INT,
-    price DECIMAL(4,2)
+    quantity INT DEFAULT 1,
+    price DECIMAL(4,2),
+    CHECK(poster_title ~ '^[A-Za-z''-]'),
+    CHECK(quantity BETWEEN 1 AND 30),
+    CHECK(price BETWEEN 6 AND 20)
 );
 
 COPY posters FROM '/Users/juanhun/Desktop/code-platoon-assignments/curriculum/05-RDBMS/1-schemadesign/example/data/poster.csv' WITH CSV HEADER;
