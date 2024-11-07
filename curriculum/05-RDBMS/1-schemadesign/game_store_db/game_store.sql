8d506a41b1a22b8b6a7f99503e31cc136966e2bc
JUAN_HUN <juanhun2424@gmail.com>
2024-11-07 09:34:03 -0500

    consilidate the drop table functionality by putting them all at the top of the page, before the first create table

--- curriculum/05-RDBMS/1-schemadesign/game_store_db/game_store.sql
+++ curriculum/05-RDBMS/1-schemadesign/game_store_db/game_store.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS employees, games, action_figures, posters ;
 CREATE TABLE employees (
     id SERIAL PRIMARY KEY,
     employee_name VARCHAR(50),
@@ -8,7 +8,6 @@ CREATE TABLE employees (
 
 COPY employees FROM '/Users/juanhun/Desktop/code-platoon-assignments/curriculum/05-RDBMS/1-schemadesign/example/data/employee.csv' WITH CSV HEADER; 
 
-DROP TABLE IF EXISTS games;
 CREATE TABLE games(
     game_id SERIAL PRIMARY KEY, 
     game_title VARCHAR(50),
@@ -18,7 +17,6 @@ CREATE TABLE games(
 
 COPY games FROM '/Users/juanhun/Desktop/code-platoon-assignments/curriculum/05-RDBMS/1-schemadesign/example/data/game.csv' WITH CSV HEADER;
 
-DROP TABLE IF EXISTS action_figures;
 CREATE TABLE action_figures(
     id SERIAL PRIMARY KEY,
     action_figure_title VARCHAR(50),
@@ -28,7 +26,6 @@ CREATE TABLE action_figures(
 
 COPY action_figures FROM '/Users/juanhun/Desktop/code-platoon-assignments/curriculum/05-RDBMS/1-schemadesign/example/data/action_figure.csv' WITH CSV HEADER;
 
-DROP TABLE IF EXISTS posters;
 CREATE TABLE posters(
     id SERIAL PRIMARY KEY,
     poster_title VARCHAR(50),
