b823cb68c9c657e0b45a1c617cca2a01bc43a320
JUAN_HUN <juanhun2424@gmail.com>
2024-11-14 10:03:05 -0500

    finally got the project to work and made first migrations

--- curriculum/07-Django/1-intro-to-django-orm/school_proj_root/student_app/models.py
+++ curriculum/07-Django/1-intro-to-django-orm/school_proj_root/student_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=255, null=False)
+    student_email = models.EmailField(max_length=255, null=False)
+    personal_email = models.EmailField(max_length=255)
+    lock_number = models.IntegerField(null=False)
+    lock_combination = models.CharField(max_length=50, null=False)
+    good_student = models.BooleanField(default=True, null=False) #this does not let it pass makemigrations make sure to fix this 
