a5436df8727dd84dc5a54a41a60675865bfba916
JUAN_HUN <juanhun2424@gmail.com>
2024-11-19 15:23:23 -0500

    initial commit, app start with project start-- .gitignore made

---
+++ curriculum/07-Django/5-back-end-apis/mapdoc_api_root/search_app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from requests_oauthlib import OAuth1
+
+
+# Create your views here.
