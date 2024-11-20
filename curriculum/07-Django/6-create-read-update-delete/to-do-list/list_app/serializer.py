8423302f4d05b4aff1eaae88dd8e3eaf015d74b2
JUAN_HUN <juanhun2424@gmail.com>
2024-11-20 17:36:06 -0500

    final commit of the day

--- curriculum/07-Django/6-create-read-update-delete/to-do-list/list_app/serializer.py
+++ curriculum/07-Django/6-create-read-update-delete/to-do-list/list_app/serializer.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
 from .models import List
-
+from to_do_app.serializers import TaskSerializer
 class ListSerializer(serializers.ModelSerializer):
+    tasks = TaskSerializer(many=True)#many=True cuz many to one--- for instances with a list of objects not just 1
     class Meta:
         model = List
-        fields = ['id', 'list_name']
+        fields = ['id', 'list_name', 'tasks']
         
-        
+        #create a converter to be able to reference 
