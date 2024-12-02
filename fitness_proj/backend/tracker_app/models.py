1c8101e678cc68a1603ac9514cf08b3bca7566c2
JUAN_HUN <juanhun2424@gmail.com>
2024-12-02 17:46:16 -0500

    link the user with the tracker utilizing a foreign key

--- fitness_proj/backend/tracker_app/models.py
+++ fitness_proj/backend/tracker_app/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from .validators import validate_tracker_name
+from user_app.models import User
 
 class Tracker(models.Model):
     tracker_name = models.CharField(blank=False, validators=[validate_tracker_name])
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_id')#this is too link the user
+    
