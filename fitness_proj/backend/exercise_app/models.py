e910925072b74d4e1dd9421bf8bc05fa53e19dea
JUAN_HUN <juanhun2424@gmail.com>
2024-12-05 14:09:32 -0500

    add weight to the models as well as reconfigured the .json files as needed

--- fitness_proj/backend/exercise_app/models.py
+++ fitness_proj/backend/exercise_app/models.py
@@ -10,4 +10,5 @@ class Exercise(models.Model):
     type = models.CharField(blank=False, validators=[validate_valid_type])
     goal_num_of_sets = models.PositiveIntegerField(blank=False, validators=[v.MinValueValidator(1)])
     goal_num_of_reps = models.PositiveIntegerField(blank=False, validators=[v.MinValueValidator(1)])
+    weight = models.PositiveIntegerField(blank=False, validators=[v.MaxValueValidator(2000)])
     workout_id = models.ForeignKey(Workout, on_delete=models.CASCADE, related_name='workouts')
